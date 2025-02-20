c15c917a3b687cf21af393c3eabc82abe57d4eee
dedekorkut35 <dedekorkut35@hotmail.com>
2025-02-21 00:11:41 +0300

    int to byte

---
+++ src/main/java/com/yusufdiler/changeinttobyte.java
@@ -0,0 +1,9 @@
+package com.yusufdiler;
+
+public class changeinttobyte {
+    public static void main(String[] args) {
+        int a=3;
+        byte b= (byte) a;
+        System.out.println(a+""+b);
+    }
+}
