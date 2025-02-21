d8f31f6faec7f641f07c77351f898ed61830249a
dedekorkut35 <dedekorkut35@hotmail.com>
2025-02-22 00:39:29 +0300

    Tarih düzenlemeleri

--- src/main/java/com/yusufdiler/Date.java
+++ src/main/java/com/yusufdiler/Date.java
@@ -6,8 +6,8 @@ public static void dateMethod () {
     java.util.Date date= new java.util.Date();
     System.out.println(date);
     System.out.println("Gün :"+date.getDate());
-    System.out.println("Ay:" +date.getDay() );
-    System.out.println("Yıl:" +date.getYear() );
+    System.out.println("Ay:" + (1 + date.getMonth() )); //ocak 0 dan başlar
+    System.out.println("Yıl:" + (1900+date.getYear()) ); //yıl için 1900 ekliyoruz
     System.out.println("Tarih:" +date.toString() );
     }
 
